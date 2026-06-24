880772cd8c6ca5d75923d876cf761a9ae29b1a21
unknown <ehijearthur@gmail.com>
2026-06-24 09:51:40 +0100

    fixed bugs

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,38 +3,34 @@ const User = require('../models/User');
 
 const protect = async (req, res, next) => {
     try {
-        let token;
-        if (
-            req.headers.authorization && 
+        const authHeader = req.headers.authorization;
 
-            req.headers.authorization.startsWith("Bearer")
-        ) {
-            token = req.headers.authorization.split(" ")[1];
-
-            const decoded = jwt.verify(
-                token,
-                process.env.JWT_SECRET
-            );
-
-            req.user = await User.findById(
-                decoded.id
-            ).select("-password");
-
-            if (req.user.isFrozen) {
-                res.status(401);
-                throw new Error("Account is frozen");
-            }
-
-            next();
-
-        } else {
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
             res.status(401);
             throw new Error("Not authorized, no token");
         }
 
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decoded.id).select("-password");
+
+        if (!user) {
+            res.status(401);
+            throw new Error("User not found");
+        }
+
+        if (user.isFrozen) {
+            res.status(401);
+            throw new Error("Account is frozen");
+        }
+
+        req.user = user;
+        next();
+
     } catch (error) {
         res.status(401);
-        throw new Error("Not authorized, invalid token");
+        next(error);
     }
 
 };
